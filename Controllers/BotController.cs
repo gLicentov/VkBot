ffea7e4c59705d4385e0ee351f58dc3ecfb37c78
gLicentov <gLicentov@gmail.com>
2025-03-26 15:59:19 +0300

    Add project files.

---
+++ Controllers/BotController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VkBot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+    }
+}
